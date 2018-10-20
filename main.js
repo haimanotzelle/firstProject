6c82e9ccd1dc7cc58a1f281d1240b80def127f4c
unknown <zellelewhaimanot9@gmail.com>
2018-10-20 10:56:10 -0700

    added pop up windows

--- main.js
+++ main.js
@@ -3,3 +3,26 @@ console.log("age is currently: "+ age);
 age = 26;
 console.log("age currently: " + age)
 age = 26 + 1;
+let name;
+console.log( "haimanot" + "zellelew")
+
+
+console.log( "hello world")
+
+
+let userName = prompt( "what is your name?");
+
+let nameConfirm = confirm("just checking, is   " + userName + "your real name?");
+
+if(nameConfirm){
+    alert("Welcome to my page, " + userName + "!");
+}
+else{
+    userName=prompt("Okay, what is your name?");
+
+}
+
+console.log("The user's name is " + userName+ ".");
+
+    
+
